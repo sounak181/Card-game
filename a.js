8b0bd092cccc0f434e086640a5075c28ed5301ac
Sounak Sutradhar <108016373+sounak181@users.noreply.github.com>
2023-06-25 12:40:14 +0530

    Add files via upload

---
+++ a.js
@@ -0,0 +1,80 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const cards = document.querySelectorAll('.card');
+    let hasFlippedCard = false;
+    let lockBoard = false;
+    let firstCard, secondCard;
+    let matchedPairs = 0;
+
+    function flipCard() {
+        if (lockBoard) return;
+        if (this === firstCard) return;
+
+        this.classList.add('flip');
+
+        if (!hasFlippedCard) {
+            hasFlippedCard = true;
+            firstCard = this;
+            return;
+        }
+
+        secondCard = this;
+        checkForMatch();
+    }
+
+    function checkForMatch() {
+        let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+        isMatch ? disableCards() : unflipCards();
+    }
+
+    function disableCards() {
+        firstCard.removeEventListener('click', flipCard);
+        secondCard.removeEventListener('click', flipCard);
+
+        matchedPairs++;
+        resetBoard();
+
+        if (matchedPairs === cards.length / 2) {
+            displayVictoryMessage();
+        }
+    }
+
+    function unflipCards() {
+        lockBoard = true;
+
+        setTimeout(() => {
+            firstCard.classList.remove('flip');
+            secondCard.classList.remove('flip');
+
+            resetBoard();
+        }, 1000);
+    }
+
+    function resetBoard() {
+        [hasFlippedCard, lockBoard] = [false, false];
+        [firstCard, secondCard] = [null, null];
+    }
+
+    function shuffleCards() {
+        cards.forEach(card => {
+            let randomPos = Math.floor(Math.random() * cards.length);
+            card.style.order = randomPos;
+        });
+    }
+
+    function startGame() {
+        cards.forEach(card => {
+            card.classList.remove('flip');
+            card.addEventListener('click', flipCard);
+            
+        });
+
+        shuffleCards();
+    }
+
+    function displayVictoryMessage() {
+        alert('Congratulations! You have matched all the pairs!');
+    }
+
+    const startButton = document.getElementById('start-btn');
+    startButton.addEventListener('click', startGame);
+});
